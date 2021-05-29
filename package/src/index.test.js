36398ea4ac416d548d1283126b18b0396c82c370
Bruno Scopelliti <bru.scopelliti@gmail.com>
2021-05-29 16:41:35 +0200

    test: add unit tests

--- package/src/index.test.js
+++ package/src/index.test.js
@@ -1,14 +1,65 @@
-import { renderHook, act } from "@testing-library/react-hooks";
-import useCounter from "./";
+import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import { render } from "@testing-library/react";
 
-describe("useCounter", () => {
-  it("should increment counter", () => {
-    const { result } = renderHook(() => useCounter());
+import useForwardRef from "./";
 
-    act(() => {
-      result.current.increment();
-    });
+/**
+ * @typedef {Object} BaseComponentProps
+ * @property {(arg : string) => void} spy
+ */
 
-    expect(result.current.count).toBe(1);
+const BaseComponent = React.forwardRef(
+  /**
+   * @param {BaseComponentProps} props
+   * @param {React.RefObject<HTMLDivElement>} maybeRef
+   */
+  (props, maybeRef) => {
+    const ref = useForwardRef(maybeRef);
+
+    useEffect(
+      () => {
+        props.spy(ref.current.id);
+      }
+    );
+
+    return (
+      <div id="test-app" ref={ref} />
+    );
+  }
+);
+
+BaseComponent.displayName = "BaseComponent";
+
+BaseComponent.propTypes = {
+  spy: PropTypes.func.isRequired,
+};
+
+const ComponentWithRef =
+  (props) => {
+    const ref = useRef();
+
+    return (
+      <BaseComponent {...props} ref={ref} />
+    );
+  };
+
+describe("useForwardRef", () => {
+  it("creates a new ref when ref is not forwarded", () => {
+    const spy = jest.fn();
+
+    render(<BaseComponent spy={spy} />);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith("test-app");
+  });
+
+  it("uses forwarded ref", () => {
+    const spy = jest.fn();
+
+    render(<ComponentWithRef spy={spy}>Demo</ComponentWithRef>);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith("test-app");
   });
 });
